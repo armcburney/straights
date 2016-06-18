d0e583de2e54fefe0547dba17a9be8a75ae93684
Mart van Buren <mart.van.buren@gmail.com>
2016-06-18 17:16:14 +0000

    Minor updates for strategy headers & created StrategyDecision object

--- AutomaticStrategy.h
+++ AutomaticStrategy.h
@@ -1,11 +1,11 @@
-#ifndef _AUTOSTRATEGY_
-#define _AUTOSTRATEGY_
+#ifndef _AUTO_PLAYER_STRATEGY_
+#define _AUTO_PLAYER_STRATEGY_
 
 #include <istream>
 #include "PlayerStrategy.h"
 
 
-class AutomaticStrategy : PlayerStrategy {
+class AutomatedPlayerStrategy : PlayerStrategy {
 
 };
 
--- PlayerStrategy.h
+++ PlayerStrategy.h
@@ -1,10 +1,16 @@
 #ifndef _PLAYERSTRATEGY_
 #define _PLAYERSTRATEGY_
 
+#include "StrategyDecision.h"
+#include "Card.h"
+
 #include <istream>
+#include <list>
+#include <vector>
 
 class PlayerStrategy {
-
+public:
+    StrategyDecision play(const std::list<Card*> &hand, const std::vector<Card*> &gamePile);
 };
 
 #endif
