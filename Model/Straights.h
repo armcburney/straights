9be787ac5ceb313f6524e98a5eee7754b0852728
Andrew R. McBurney <mcburney.andrew13@gmail.com>
2016-06-19 23:44:21 -0400

    a

--- Model/Straights.h
+++ Model/Straights.h
@@ -19,24 +19,90 @@ class Straights {
 
 public:
     Straights();
-
+    /*
+    POSTCONDITIONS:
+      - ensures: initializes this to Straights with seed = 0, empty gamePile, full
+      deck, and no players
+    */
     void addHumanPlayer(int);
+    /*
+    POSTCONDITIONS:
+      - modifies: this
+      - ensures: players with new player having id passed in
+    */
     void addComputerPlayer(int);
+    /*
+    POSTCONDITIONS:
+      - modifies: this
+      - ensures: players with new player having id passed in
+    */
     void automateCurrentPlayer();
+    /*
+    POSTCONDITIONS:
+      - modifies: currentPlayer
+      - ensures: currentPlayer has AutomatedPlayerStrategy
+    */
     PlayerItr returnCurrentPlayer() const;
+    /*
+    POSTCONDITIONS:
+      - return: currentPlayer
+    */
 
     void clearRound();
+    /*
+    POSTCONDITIONS:
+      - modifies: this
+      - ensures: the game pile is emptied and players hands and discard piles are emptied
+    */
     void deal();
+    /*
+    POSTCONDITIONS:
+      - modifies: players, deck
+      - ensures: shuffles the deck and evenly distributes them amongst players
+    */
     void setShuffleSeed(int);
+    /*
+    PRECONDITION:
+      - requires: an integer passed in
+    POSTCONDITIONS:
+      - modifies: seed
+      - ensures: updates seed to the value passed in
+    */
 
     TurnResult next(const Command &input = Command());
+    /*
+    PRECONDITION:
+      - requires: players
+    POSTCONDITIONS:
+      - modifies: this
+      - ensures: currentPlayer plays their strategy, the game and round are checked
+      for completion
+    */
     TurnContext getTurnContext() const;
+    /*
+    POSTCONDITIONS:
+      - returns: an object describing the game pile and player's hand
+    */
     RoundContext getRoundContext() const;
+    /*
+    POSTCONDITIONS:
+      - returns: an object describing the current round
+    */
     Deck getDeck() const;
+    /*
+    POSTCONDITIONS:
+      - returns: the deck
+    */
 
     static std::vector<CardPtr> getLegalPlays(
         std::list<CardPtr> hand,
         const std::set<CardPtr, CardPtrComp> &gamePile);
+    /*
+    PRECONDITIONS:
+      - requires: the hand is not empty
+    POSTCONDITIONS:
+      - returns: all possible legal moves given the hand and game pile
+    */
 private:
     int seed_;
     Deck deck;
