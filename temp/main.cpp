f625bfd19fca406edb747ce34fb403aa4b14b255
armcburn <armcburn@student.cs.uwaterloo.ca>
2016-07-15 14:43:58 -0400

    Tried a bunch of things for signals, no luck so far. Documentation is terrible. Gtk Why???

--- temp/main.cpp
+++ temp/main.cpp
@@ -2,19 +2,22 @@
 #include <gtkmm/builder.h>
 #include <gtkmm/main.h>
 #include <gtkmm/window.h>
+#include <gtkmm/button.h>
 
-G_MODULE_EXPORT void button1_cb_clicked ( GtkButton *button, gpointer data) {
-  std::cout << "test this out" << std::endl;
+void test () {
+  std::cout << "is this working? lmao" << std::endl;
 }
 
 int main (int argc, char** argv) {
-  std::cout << "Init..." << std::endl;
   Gtk::Main kit(argc, argv);
   Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("main.glade");
   Gtk::Window* window;
   builder->get_widget("window1", window);
-  std::cout << "Set up window..." << std::endl;
   window->show();
   kit.run();
-}
 
+  Gtk::Button * button2;
+  builder->get_widget("button2", button2);
+  button2->signal_clicked().connect( sigc::ptr_fun(&test) );
+ // button1->signal_clicked().connect(sigc::mem_fun(*this, &test));
+}
